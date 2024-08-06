5f367f3976e3f401f7e581e286139b34b6ebc48b
Ashish Kumar <ashishableo12@gmail.com>
2024-08-07 00:07:14 +0530

    second

--- src/app/Upvote.js
+++ src/app/Upvote.js
@@ -1,41 +1,3 @@
-// "use client"; // Add this line at the top of your file
-
-// import React, { useState } from 'react';
-// import Dropdown from './Dropdown';
-// import { OptionsDropdown } from '@mantine/core';
-// import { FaThumbsUp } from 'react-icons/fa'; // Example icon from react-icons library
-
-// const Upvote = () => {
-//   const [counter, setCounter] = useState(0);
-
-//   const increaseCounter = () => {
-//     setCounter(counter + 1);
-//   };
-
-//   return (
-//     <div className="p-4">
-//       <div className="mb-4">
-//         Translate videos in your own voice, globalize in a click!
-//       </div>
-//       <div className="flex space-x-4">
-//         <button className="px-4 py-2 rounded">
-//           <p>Visit</p>
-//         </button>
-//         <button 
-//           className=" bg-orange-600 text-white px-4 py-2 rounded flex items-center space-x-2" 
-//           onClick={increaseCounter}
-//         >
-//           <FaThumbsUp className="h-5 w-5" />
-//           <p>UPVOTE</p>
-//           <p>{counter}</p>
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Upvote;
-
 "use client"; // Ensure this is a client component
 
 import React, { useState } from 'react';
@@ -44,9 +6,11 @@ import DropdownMenu from './DropdownMenu'; // Import the DropdownMenu component
 
 const Upvote = () => {
   const [counter, setCounter] = useState(0);
+  const [isUpvoted, setIsUpvoted] = useState(false); // New state to track upvote status
 
   const increaseCounter = () => {
     setCounter(counter + 1);
+    setIsUpvoted(!isUpvoted); // Toggle upvote status
   };
 
   return (
@@ -57,7 +21,7 @@ const Upvote = () => {
       <div className="flex space-x-4">
         <DropdownMenu /> {/* Use the DropdownMenu component here */}
         <button 
-          className=" bg-orange-500 text-white px-4 py-2 rounded flex items-center space-x-2" 
+          className={`px-4 py-2 rounded flex items-center space-x-2 ${isUpvoted ? 'bg-white text-black border border-black' : 'bg-orange-500 text-white'}`} 
           onClick={increaseCounter}
         >
           <FaThumbsUp className="h-5 w-5" />
